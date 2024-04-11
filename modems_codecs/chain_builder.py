2b4a7cb210f4cfc69496b4e0ea241bb68822a5b6
Nino Carrillo <nino.carrillo@outlook.com>
2024-04-11 15:02:24 -0400

    add fsk

--- modems_codecs/chain_builder.py
+++ modems_codecs/chain_builder.py
@@ -5,7 +5,7 @@
 
 import modems_codecs.psk
 import modems_codecs.afsk
-import modems_codecs.gfsk
+import modems_codecs.fsk
 import modems_codecs.lfsr
 import modems_codecs.slicer
 import modems_codecs.il2p
@@ -18,8 +18,8 @@ def ModemConfigurator(arg_sample_rate, input_args):
 		if input_args['type'] == 'bpsk':
 			new_object = modems_codecs.psk.BPSKModem(sample_rate=arg_sample_rate, config=input_args['config'])
 			new_object.StringOptionsRetune(input_args['options'])
-		elif input_args['type'] == 'gfsk':
-			new_object = modems_codecs.gfsk.GFSKModem()
+		elif input_args['type'] == 'fsk':
+			new_object = modems_codecs.fsk.FSKModem(sample_rate=arg_sample_rate, config=input_args['config'])
 		elif input_args['type'] == 'afsk':
 			new_object = modems_codecs.afsk.AFSKModem(sample_rate=arg_sample_rate, config=input_args['config'])
 			new_object.StringOptionsRetune(input_args['options'])
