8311ac8de1d96fb1f3ccf6a3b116a3f69d232659
Nino Carrillo <nino.carrillo@outlook.com>
2024-05-22 21:43:30 -0400

    tune mpsk4 max freq offsets

--- modems_codecs/psk.py
+++ modems_codecs/psk.py
@@ -543,7 +543,7 @@ class MPSKModem:
 			self.input_bpf_span = 2.7			# milliseconds spanned by input filter
 			self.hilbert_span = 2.7			# milliseconds spanned by hilbert transformer
 			self.carrier_freq = 1650.0				# carrier tone frequency
-			self.max_freq_offset = 150
+			self.max_freq_offset = 60
 			self.rrc_rolloff_rate = 0.3
 			self.rrc_span = 8
 			self.Loop_LPF = IIR_1(
@@ -571,7 +571,7 @@ class MPSKModem:
 			self.input_bpf_span = 2.7			# milliseconds spanned by input filter
 			self.hilbert_span = 2.7			# milliseconds spanned by hilbert transformer
 			self.carrier_freq = 1500.0				# carrier tone frequency
-			self.max_freq_offset = 40
+			self.max_freq_offset = 30
 			self.rrc_rolloff_rate = 0.6
 			self.rrc_span = 6
 			self.Loop_LPF = IIR_1(
